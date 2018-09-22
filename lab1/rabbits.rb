b3db5a919d0885bdad085314200e5c0e23c3f1c2
ProkopenkoRoman <long399@mail.ru>
2018-09-22 10:57:00 +0700

    Update rabbits.rb

--- lab1/rabbits.rb
+++ lab1/rabbits.rb
@@ -8,6 +8,10 @@ end
 
 puts 'Input number of months: '
 month = gets.chomp
+if month.to_i == 0 || month.to_i < 1
+	puts 'Invalid value.. Try again!'
+	exit
+end
 
 puts 'couples of rabbits: '  
 puts fibonacci(month.to_i + 2)
