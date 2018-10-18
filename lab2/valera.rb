7de53e49eff68e7988b207f8835bad8d89331892
ProkopenkoRoman <long399@mail.ru>
2018-10-18 10:29:04 +0700

    Update valera.rb

--- lab2/valera.rb
+++ lab2/valera.rb
@@ -1,12 +1,12 @@
 require 'yaml'
 
 class Valera
-  attr_accessor :health 	#здоровье 				(0..100)
-  attr_accessor :mana 		#мана(алкоголь) 			(0..100)
-  attr_accessor :money 		#деньги
-  attr_accessor :happiness 	#жизнерадостность 			(-10..10)
-  attr_accessor :fatigue 	#усталость 				(0..100)
-  attr_accessor :dead 		#мертв?
+  attr_accessor :health 	  #здоровье 				  (0..100)
+  attr_accessor :mana 		  #мана(алкоголь) 		(0..100)
+  attr_accessor :money 		  #деньги
+  attr_accessor :happiness 	#жизнерадостность 	(-10..10)
+  attr_accessor :fatigue 	  #усталость 				  (0..100)
+  attr_accessor :dead 		  #мертв?
 
   def initialize (args)
     @health = args[:health]
