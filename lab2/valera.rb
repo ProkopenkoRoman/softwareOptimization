4f2ce302c5655e22786c00662ddb6736f1fb5d3a
ProkopenkoRoman <long399@mail.ru>
2018-10-18 08:39:49 +0700

    Update valera.rb

--- lab2/valera.rb
+++ lab2/valera.rb
@@ -2,9 +2,9 @@ require 'yaml'
 
 class Valera
   attr_accessor :health 	#здоровье 			(0..100)
-  attr_accessor :mana 		#мана(алкоголь) 	(0..100)
+  attr_accessor :mana 		#мана(алкоголь) 		(0..100)
   attr_accessor :money 		#деньги
-  attr_accessor :happiness 	#жизнерадостность 	(-10..10)
+  attr_accessor :happiness 	#жизнерадостность 		(-10..10)
   attr_accessor :fatigue 	#усталость 			(0..100)
   attr_accessor :dead 		#мертв?
 
